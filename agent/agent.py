4d8dde1945f2aeedec3484af062a6c6401c0c897
Mario Alberich <malberich@gmail.com>
2018-04-28 12:16:50 +0200

    Added some basic actions, renamed expReplay, imported functions to use xrange on Python 3

--- agent/agent.py
+++ agent/agent.py
@@ -7,54 +7,70 @@ from keras.optimizers import Adam
 import numpy as np
 import random
 from collections import deque
+from functions_dataframe import *
 
 class Agent:
-	def __init__(self, state_size, is_eval=False, model_name=""):
-		self.state_size = state_size # normalized previous days
-		self.action_size = 3 # sit, buy, sell
-		self.memory = deque(maxlen=1000)
-		self.inventory = []
-		self.model_name = model_name
-		self.is_eval = is_eval
 
-		self.gamma = 0.95
-		self.epsilon = 1.0
-		self.epsilon_min = 0.01
-		self.epsilon_decay = 0.995
+    ACTION_SIT = 0
+    ACTION_BUY = 1
+    ACTION_SELL = 2
+    ACTION_WITHDRAW1 = 3
+    ACTION_REFUND1 = 4
 
-		self.model = load_model("models/" + model_name) if is_eval else self._model()
+    def __init__(self, state_size, is_eval=False, model_name=""):
+        self.state_size = state_size # normalized previous days
+        self.action_size = 3 # sit, buy, sell
+        self.memory = deque(maxlen=1000)
+        self.inventory = []
+        self.model_name = model_name
+        self.is_eval = is_eval
 
-	def _model(self):
-		model = Sequential()
-		model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
-		model.add(Dense(units=32, activation="relu"))
-		model.add(Dense(units=8, activation="relu"))
-		model.add(Dense(self.action_size, activation="linear"))
-		model.compile(loss="mse", optimizer=Adam(lr=0.001))
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
 
-		return model
+        self.model = load_model("models/" + model_name) if is_eval else self._model()
 
-	def act(self, state):
-		if not self.is_eval and np.random.rand() <= self.epsilon:
-			return random.randrange(self.action_size)
+    def _model(self):
+        model = Sequential()
+        model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(units=32, activation="relu"))
+        model.add(Dense(units=8, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(lr=0.001))
 
-		options = self.model.predict(state)
-		return np.argmax(options[0])
+        return model
 
-	def expReplay(self, batch_size):
-		mini_batch = []
-		l = len(self.memory)
-		for i in xrange(l - batch_size + 1, l):
-			mini_batch.append(self.memory[i])
+    def act(self, state):
+        if not self.is_eval and np.random.rand() <= self.epsilon:
+            return (
+                '',
+                random.randrange(self.action_size),
+                [1 / self.action_size] * self.action_size
+            )
 
-		for state, action, reward, next_state, done in mini_batch:
-			target = reward
-			if not done:
-				target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+        options = self.model.predict(state)
+        return (
+            '*',
+            np.argmax(options[0]),
+            options[0]
+        )
 
-			target_f = self.model.predict(state)
-			target_f[0][action] = target
-			self.model.fit(state, target_f, epochs=1, verbose=0)
+    def experiment_replay(self, batch_size):
+        mini_batch = []
+        l = len(self.memory)
+        for i in xrange(l - batch_size + 1, l):
+            mini_batch.append(self.memory[i])
 
-		if self.epsilon > self.epsilon_min:
-			self.epsilon *= self.epsilon_decay 
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
